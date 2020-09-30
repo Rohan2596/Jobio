2b02b1d5357650385ff8961c315f6d90871b55ba
Rohan2596 <rohankadam662@gmail.com>
2020-09-30 16:42:05 +0530

    ADDED:Toolbar to applications.

--- app/src/main/java/com/spatalabz/jobio/MainActivity.java
+++ app/src/main/java/com/spatalabz/jobio/MainActivity.java
@@ -3,12 +3,19 @@ package com.spatalabz.jobio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+
     }
 }

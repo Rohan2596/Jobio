0788d9ed8f5b72894c3e69ea3fce9a2307dbc90a
Rohan2596 <rohankadam662@gmail.com>
2020-10-02 18:07:52 +0530

    ADDED:Job Listing Activity to Jobio Application.

---
+++ app/src/main/java/com/spatalabz/jobio/JobListing.java
@@ -0,0 +1,14 @@
+package com.spatalabz.jobio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JobListing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_listing);
+    }
+}

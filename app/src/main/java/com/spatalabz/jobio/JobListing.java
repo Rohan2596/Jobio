41ef45759dccfd5cca8ec09fcc11fb622a73657b
Rohan2596 <rohankadam662@gmail.com>
2020-10-03 15:57:29 +0530

    ADDED:Recycler view and Layout manager to application.

--- app/src/main/java/com/spatalabz/jobio/JobListing.java
+++ app/src/main/java/com/spatalabz/jobio/JobListing.java
@@ -1,14 +1,36 @@
 package com.spatalabz.jobio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.spatalabz.jobio.model.Job;
+
+import java.util.ArrayList;
+
 public class JobListing extends AppCompatActivity {
+
+    RecyclerView job_recycler_view;
+    ArrayList<Job>jobs=new ArrayList<>();
+
+    RecyclerView.LayoutManager  layoutManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_listing);
+        /*
+        * Jobs Recycler View
+        * */
+        job_recycler_view=findViewById(R.id.job_recycler_view);
+        layoutManager=new LinearLayoutManager(JobListing.this,LinearLayoutManager.VERTICAL,false);
+        job_recycler_view.setLayoutManager(layoutManager);
+
+
+
     }
 }
